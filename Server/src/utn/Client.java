8fa3c1f986fd2f043bc2eeed2c643c602ead7c65
FeDeJMendez <fedemendezg@gmail.com>
2022-05-23 21:04:16 -0300

    Add connection messages and hostname

--- Server/src/utn/Client.java
+++ Server/src/utn/Client.java
@@ -19,15 +19,34 @@ public class Client extends Thread { //también se puede usar Runnable
         try {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("IP: " + clientSocket.getInetAddress());
-            String msg = ""; //;
+//            System.out.println("Client " + clientSocket.getInetAddress().getHostName() + " connected!");
+            out.println("Service Connected!");
+            String msg = "";
+            String name = "";
+            out.println("Specified your name: ");
+            name = in.readLine();
+            System.out.println("Client " + name + " connected!");
             do{
-                if(!msg.equals(""))
-                    System.out.println(msg);
+                if(msg.equals("hola")){
+                    out.println("hola como andas?");
+                    continue;
+                }
+                if(msg.equals("bien y vos?")){
+                    out.println("bien todo bien...");
+                    continue;
+                }
+                if(!msg.equals("")) {
+//                    System.out.println(clientSocket.getInetAddress().getHostAddress() + " says: " + msg);
+                    System.out.println(name + " says: " + msg);
+                    out.println("response: " + msg);
+                }
             } while(!(msg = in.readLine()).equals("x"));
+            out.println("Service Disconected!");
             clientSocket.close();
             out.close();
             in.close();
+//            System.out.println("Client " + clientSocket.getInetAddress().getHostAddress() + " disconected!");
+            System.out.println("Client " + name + " disconected!");
         } catch (IOException e) {
             e.printStackTrace();
         }
